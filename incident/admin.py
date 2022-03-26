8af0a14878d694ea263ea68bc18c55607b60a085
Lawwest88 <lawrence-west@hotmail.com>
2022-03-26 15:11:05 -0500

    register incident/reporter models

--- incident/admin.py
+++ incident/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from incident.models import Incident_type
 
-# Register your models here.
+class IncidentAdmin(admin.ModelAdmin):
+    list_display = ('fire','medical','police','created')
+
+admin.site.register(Incident_type, IncidentAdmin)
--- reporter/admin.py
+++ reporter/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from reporter.models import Reporter
 
-# Register your models here.
+class ReporterAdmin(admin.ModelAdmin):
+    list_display = ('first_name','last_name','age')
+
+admin.site.register(Reporter, ReporterAdmin)
