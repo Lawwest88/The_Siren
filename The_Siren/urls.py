0b0bd58fb34c80b218b634ccf07896a44e338201
Lawwest88 <lawrence-west@hotmail.com>
2022-03-26 17:37:27 -0500

    changes to common file

--- The_Siren/urls.py
+++ The_Siren/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from turtle import home
 from django.contrib import admin
 from django.urls import path
 
+from common.views import HomeView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name='home'),
 ]
