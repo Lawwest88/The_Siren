e7cc7aaf6c7202ab0070f159a73e8e86e5ec2600
Lawwest88 <lawrence-west@hotmail.com>
2022-03-26 02:57:31 -0500

    updated incident model

--- incident/models.py
+++ incident/models.py
@@ -4,3 +4,4 @@ class Incident_type(models.Model):
     fire = models.CharField(max_length=50)
     police = models.CharField(max_length=50)
     medical = models.CharField(max_length=50)
+    created = models.DateTimeField(auto_now=True)
