f6821a39e42a004d91cc153788cd1c2602da8a68
Lawwest88 <lawrence-west@hotmail.com>
2022-04-06 19:30:06 -0500

    create profile mpdel

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=False)
+    handle = models.CharField(max_length=120, blank=True)
+
+    # Contact Info
+    phone_number = models.CharField(max_length=20, blank=True)
+
+    # Location
+    country = models.CharField(max_length=120, blank=False)
+    province = models.CharField(max_length=250, blank=False)
+    postal = models.CharField(max_length=30, blank=False)
+    city = models.CharField(max_length=250, blank=False)
+
+    
